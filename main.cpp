9d0d925a6792cd9c30883bf27d89487bfd62a269
Joice Beatriz Paim Sanchez <54649098+joicepaim@users.noreply.github.com>
2023-01-03 11:35:36 -0300

    1047 - Tempo de Jogo com Minutos

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int h1,m1,h2,m2;
+    int x,y;
+    
+    cin >> h1 >> m1 >> h2 >> m2;
+    
+    if(h1 == h2){
+        if(m1 == m2){
+            x = 24;
+            y = 0;
+        }else{
+            if(m2 < m1){
+                x = 23;
+                y = 60 - (m1-m2);
+            }else{
+                x = 0;
+                y = m2 - m1;
+            }
+            
+        }
+    }else if(h1<h2){
+       x = (((h2*60)+m2) - ((h1*60)+m1))/60;
+       y = (((h2*60)+m2) - ((h1*60)+m1))%60;
+      
+    }else{
+       x = (1440 - (((h1*60)+m1) - ((h2*60)+m2)) )/60;
+       y = (1440 - (((h1*60)+m1) - ((h2*60)+m2)) )%60;
+    }
+    cout<<"O JOGO DUROU "<<x<<" HORA(S) E "<<y<<" MINUTO(S)"<<endl;
+    return 0;
+}
